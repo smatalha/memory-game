c3935be91533a1f4bb899aa4dfc96def3be10482
smatalha <sma.talha@gmail.com>
2020-03-23 06:03:15 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,17 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; 
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 console.log("user flipped " + cardOne);
-console.log("user flipped " + cardThree);
+console.log("user flipped " + cardTwo);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match");
+} else {
+	alert("Sorry, try again");
+}
